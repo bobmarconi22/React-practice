b97530c6c6c9c98579e07eace9d897532d5fc4e5
Bob <bobmarconi22@gmail.com>
2025-01-03 18:03:12 -0500

    need API

--- my-react-app/src/Components/Stats.js
+++ my-react-app/src/Components/Stats.js
@@ -8,7 +8,7 @@ function Baseball() {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          "https://statsapi.mlb.com/api/v1/standings?leagueId=103,104&season=2024&standingsTypes=regularSeason"
+          "https://"
         );
         if (!response.ok) {
           throw new Error("Failed Network Response: " + response.statusText);
